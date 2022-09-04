79e90f6eb4151343a67e686ba150ccf80eb87d7a
eChadwick <en.chadwick@gmail.com>
2022-09-04 16:04:11 -0500

    add Ingredient model and empty test

--- RecipeBox/RecipeBox/settings.py
+++ RecipeBox/RecipeBox/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'recipe_app.apps.RecipeAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- RecipeBox/recipe_app/tests.py
+++ RecipeBox/recipe_app/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
+from recipe_app.models import Ingredient
 
 # Create your tests here.
