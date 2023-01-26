9148e75194e0e9120ba91c488863b1ad664b8471
eChadwick <en.chadwick@gmail.com>
2023-01-25 21:39:04 -0600

    title case ingredient names in __init__ instead of save

--- RecipeBox/recipe_app/models.py
+++ RecipeBox/recipe_app/models.py
@@ -7,8 +7,11 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.CharField(null=False, max_length=200, unique=True)
 
-    def save(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.name = self.name.title()
+
+    def save(self, *args, **kwargs):
         try:
             self.validate_unique()
         except ValidationError:
