38ca4b37227c64ccd1426b632a3b0e250cc73dea
eChadwick <en.chadwick@gmail.com>
2023-01-26 20:47:02 -0600

    title case recipe names in init instead of save

--- RecipeBox/recipe_app/models.py
+++ RecipeBox/recipe_app/models.py
@@ -23,7 +23,7 @@ class Ingredient(models.Model):
 class Recipe(models.Model):
     name = models.CharField(null=False, max_length=200, unique=True)
 
-    def save(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         self.name = self.name.title()
 
-        models.Model.save(self, *args, **kwargs)
