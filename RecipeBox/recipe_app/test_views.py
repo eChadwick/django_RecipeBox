eeb7d87a588861b835ce44281c273b8bddcfefcd
eChadwick <en.chadwick@gmail.com>
2023-03-06 21:18:59 -0600

    implement ingredients_list view

--- RecipeBox/recipe_app/test_views.py
+++ RecipeBox/recipe_app/test_views.py
@@ -2,16 +2,25 @@ from django.test import TestCase
 from django.urls import reverse
 from recipe_app.models import Ingredient
 
-class IngredientViewTests(TestCase):
 
-    def setupTestData(self):
-        for x in range(5):
+class IngredientViewTests(TestCase):
+    num_test_ingredients = 10
+
+    def setUp(self):
+        for x in range(self.num_test_ingredients):
             Ingredient.objects.create(name=f'Ingredient {x}')
 
     def test_ingredients_list_path(self):
         response = self.client.get('/ingredients/')
         self.assertEqual(response.status_code, 200)
 
-    def test_ingredients_list_name(self):
+    def test_ingredients_list_route_name(self):
         response = self.client.get(reverse('ingredients'))
         self.assertEqual(response.status_code, 200)
+
+    def test_list_all_ingredients(self):
+        response = self.client.get(reverse('ingredients'))
+        self.assertEqual(
+            len(response.context['ingredients_list']),
+            self.num_test_ingredients
+        )
--- RecipeBox/recipe_app/views.py
+++ RecipeBox/recipe_app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from recipe_app.models import Ingredient
+
 # Create your views here.
 
 def index(request):
     return HttpResponse("Recipe Index Page!!!")
 
 def ingredient_list(request):
-    return HttpResponse('Ingredients List Page')
+    ingredients_list = list(Ingredient.objects.all())
+    context = {
+        'ingredients_list': ingredients_list
+    }
+    return render(request,'recipe_app/ingredient_list.html', context)
