327cc4e6691fce74869615b223d62c3168919eaa
eChadwick <en.chadwick@gmail.com>
2023-03-07 20:37:59 -0600

    add num_pages to ingredient list response

--- RecipeBox/recipe_app/test_views.py
+++ RecipeBox/recipe_app/test_views.py
@@ -1,10 +1,14 @@
+import math
+
 from django.test import TestCase
 from django.urls import reverse
+
 from recipe_app.models import Ingredient
+from recipe_app.views import DEFAULT_PAGINATION
 
 
 class IngredientViewTests(TestCase):
-    num_test_ingredients = 10
+    num_test_ingredients = DEFAULT_PAGINATION + 5
 
     def setUp(self):
         for x in range(self.num_test_ingredients):
@@ -18,9 +22,18 @@ class IngredientViewTests(TestCase):
         response = self.client.get(reverse('ingredients'))
         self.assertEqual(response.status_code, 200)
 
-    def test_list_all_ingredients(self):
+    def test_ingredients_list_has_number_of_pages(self):
         response = self.client.get(reverse('ingredients'))
-        self.assertEqual(
-            len(response.context['ingredients_list']),
-            self.num_test_ingredients
-        )
+        num_pages = response.context['num_pages']
+
+        total_ingredients = Ingredient.objects.all().count()
+        expected_num_pages = math.ceil(total_ingredients / DEFAULT_PAGINATION)
+
+        self.assertEqual(num_pages, expected_num_pages)
+
+    # def test_ingredients_list_default_pagination(self):
+    #     response = self.client.get(reverse('ingredients'))
+    #     self.assertEqual(
+    #         len(response.context['ingredients_list']),
+    #         DEFAULT_PAGINATION
+    #     )
--- RecipeBox/recipe_app/views.py
+++ RecipeBox/recipe_app/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 
 from recipe_app.models import Ingredient
 
-# Create your views here.
+DEFAULT_PAGINATION = 25
+
 
 def index(request):
     return HttpResponse("Recipe Index Page!!!")
 
+
 def ingredient_list(request):
     ingredients_list = list(Ingredient.objects.all())
+    paginator = Paginator(ingredients_list, DEFAULT_PAGINATION)
     context = {
-        'ingredients_list': ingredients_list
+        'ingredients_list': ingredients_list,
+        'num_pages': paginator.num_pages
     }
-    return render(request,'recipe_app/ingredient_list.html', context)
+    return render(request, 'recipe_app/ingredient_list.html', context)
