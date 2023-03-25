42d714c0cf6d30b936d2f2a9d83315fc104046ba
eChadwick <en.chadwick@gmail.com>
2023-03-24 20:37:57 -0500

    rename urls to recipe_urls

---
+++ RecipeBox/recipe_app/recipe_urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.recipe_list , name='recipe-list'),
+    path('delete/<int:pk>/', views.recipe_delete, name='recipe-delete'),
+    path('detail/<int:pk>/', views.recipe_detail, name='recipe-detail')
+]
--- RecipeBox/recipe_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.recipe_list , name='recipe-list'),
-    path('recipes/delete/<int:pk>/', views.recipe_delete, name='recipe-delete'),
-    path('recipes/detail/<int:pk>/', views.recipe_detail, name='recipe-detail')
-]
