4bcc835e9e9b83bfce5037dfaa9cee9fa386099c
eChadwick <en.chadwick@gmail.com>
2023-06-06 21:44:26 -0500

    add models to admin site

--- RecipeBox/recipe_app/admin.py
+++ RecipeBox/recipe_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from recipe_app.models import Recipe, Ingredient, RecipeIngredient
 
 # Register your models here.
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(RecipeIngredient)
