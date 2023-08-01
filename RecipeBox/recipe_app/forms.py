9f374c09708c62bd53d6525d0fcf60065dcbfe89
eChadwick <en.chadwick@gmail.com>
2023-07-31 21:37:41 -0500

    new recipe form using ModelForm

--- RecipeBox/recipe_app/forms.py
+++ RecipeBox/recipe_app/forms.py
@@ -1,43 +1,21 @@
-from django import forms
+from django.forms import Form, ModelForm, CharField, formset_factory
+
+from recipe_app.models import Recipe
 
 
-class IngredientForm(forms.Form):
+class IngredientForm(Form):
     name_validation_error = 'Ingredient name is required'
-    name = forms.CharField(max_length=255, required=True, error_messages={
+    name = CharField(max_length=255, required=True, error_messages={
         'required': name_validation_error})
-    measurement = forms.CharField(max_length=255, required=False)
+    measurement = CharField(max_length=255, required=False)
 
 
 extra_ingredient_form_count = 1
-IngredientFormSet = forms.formset_factory(
+IngredientFormSet = formset_factory(
     IngredientForm, extra=extra_ingredient_form_count)
 
 
-class RecipeForm(forms.Form):
-    name_validation_error = 'Recipe name is required'
-    ingredient_error = 'Ingredient errors are present'
-    content_validation_error = 'Ingredients or directions must be provided'
-    name = forms.CharField(
-        max_length=255,
-        required=True,
-        error_messages={
-            'required': 'Recipe name is required'
-        }
-    )
-    directions = forms.CharField(max_length=10000, required=False)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if args and 'ingredients' in args[0]:
-            self.ingredients = IngredientFormSet(args[0]['ingredients'])
-        else:
-            self.ingredients = IngredientFormSet()
-
-    def clean(self):
-        super().clean()
-        ingredients_valid = self.ingredients.is_valid()
-        if (self.ingredients.is_bound and not ingredients_valid):
-            self.add_error(None, self.ingredient_error)
-
-        if (not self.ingredients.is_bound and not self.cleaned_data['directions']):
-            self.add_error(None, self.content_validation_error)
+class RecipeForm(ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ['name', 'directions']
--- RecipeBox/recipe_app/test_forms.py
+++ RecipeBox/recipe_app/test_forms.py
@@ -4,6 +4,7 @@ from django.forms import formset_factory
 from django.test import TestCase
 
 from recipe_app.forms import IngredientForm, RecipeForm, IngredientFormSet, extra_ingredient_form_count
+from recipe_app.models import Recipe
 
 
 class IngredientFormTests(TestCase):
@@ -26,66 +27,11 @@ class IngredientFormsetTests(TestCase):
 
 class RecipeFormTests(TestCase):
 
-    def test_recipe_name_is_required(self):
-        form = RecipeForm({})
-        self.assertFalse(form.is_valid())
-        self.assertIn(RecipeForm.name_validation_error, form.errors['name'])
-
-    def test_form_instantiates_bound_formset_when_data_passed(self):
-        ingredients = {
-            'form-TOTAL_FORMS': '1',
-            'form-INITIAL_FORMS': '0',
-            'form-0-name': 'ingredient',
-            'form-0-measurement': 'measurement',
-        }
-        recipe_form = RecipeForm(
-            {'name': 'test name', 'ingredients': ingredients})
-
-        self.assertTrue(recipe_form.ingredients.is_bound)
-        self.assertIsInstance(recipe_form.ingredients, IngredientFormSet)
-        self.assertEqual(
-            recipe_form.ingredients.cleaned_data[0]['name'], ingredients['form-0-name'])
-        self.assertEqual(
-            recipe_form.ingredients.cleaned_data[0]['measurement'], ingredients['form-0-measurement'])
-
-    def test_form_instantiates_unbound_formset_when_no_data_passed(self):
+    def test_fields(self):
         form = RecipeForm()
-        self.assertFalse(form.ingredients.is_bound)
-        self.assertIsInstance(form.ingredients, IngredientFormSet)
-
-    def test_form_validates_ingredients_on_clean(self):
-        form = RecipeForm({})
-        form.ingredients.is_valid = MagicMock()
-        form.is_valid()
-        form.ingredients.is_valid.assert_called()
-
-    def test_form_invalid_when_ingredients_invalid(self):
-        form = RecipeForm({})
-        form.ingredients.is_valid = MagicMock(return_value=False)
-        form.ingredients.is_bound = True
-        self.assertFalse(form.is_valid())
-        self.assertIn(RecipeForm.ingredient_error, form.non_field_errors())
-
-    def test_directions_or_ingredients_required(self):
-        ingredients = {
-            'form-TOTAL_FORMS': '1',
-            'form-INITIAL_FORMS': '0',
-            'form-0-name': 'ingredient',
-            'form-0-measurement': 'measurement',
-        }
-
-        form = RecipeForm({'name': 'test name',
-                           'directions': 'test directions', 'ingredients': ingredients})
-        self.assertTrue(form.is_valid())
-
-        form = RecipeForm(
-            {'name': 'test name', 'directions': 'test directions'})
-        self.assertTrue(form.is_valid())
-
-        form = RecipeForm({'name': 'test name', 'ingredients': ingredients})
-        self.assertTrue(form.is_valid())
-
-        form = RecipeForm({'name': 'test name'})
-        self.assertFalse(form.is_valid())
-        self.assertIn(RecipeForm.content_validation_error,
-                      form.errors['__all__'])
+        self.assertIs(
+            type(form.instance),
+            Recipe
+        )
+        self.assertIn('name', form.fields)
+        self.assertIn('directions', form.fields)
