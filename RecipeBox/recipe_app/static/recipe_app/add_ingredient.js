53b50ce1c9358f10b478de5de7e2db8a1996b92f
eChadwick <en.chadwick@gmail.com>
2024-01-09 20:34:53 -0600

    fix alignment in recipe_form.html

--- RecipeBox/recipe_app/static/recipe_app/add_ingredient.js
+++ RecipeBox/recipe_app/static/recipe_app/add_ingredient.js
@@ -1,12 +1,13 @@
 function addIngredient() {
-    var newDiv = document.createElement('div');
+  var newDiv = document.createElement('div');
 
-    newDiv.innerHTML = `
+  newDiv.innerHTML = `
     <input type="text" name="form-${document.getElementById('id_form-TOTAL_FORMS').value}-name" placeholder="Ingredient" maxlength="255" id="id_form-${document.getElementById('id_form-TOTAL_FORMS').value}-name">
+    -
     <input type="text" name="form-${document.getElementById('id_form-TOTAL_FORMS').value}-measurement" placeholder="Amount" maxlength="255" id="id_form-${document.getElementById('id_form-TOTAL_FORMS').value}-measurement">
     <input type="checkbox" name="form-${document.getElementById('id_form-TOTAL_FORMS').value}-DELETE" onclick="hideParent(this)" id="id_form-${document.getElementById('id_form-TOTAL_FORMS').value}-DELETE">
   `;
 
-    document.getElementsByClassName('ingredients-pane')[0].appendChild(newDiv);
-    document.getElementById('id_form-TOTAL_FORMS').value++
+  document.getElementsByClassName('ingredients-pane')[0].appendChild(newDiv);
+  document.getElementById('id_form-TOTAL_FORMS').value++
 }
