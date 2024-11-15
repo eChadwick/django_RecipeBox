61a6ab54660e4b09f7972ba754f2847cec50591d
eChadwick <en.chadwick@gmail.com>
2024-11-15 10:14:26 -0600

    minor refactor in add_ingredient.js

--- RecipeBox/recipe_app/static/recipe_app/add_ingredient.js
+++ RecipeBox/recipe_app/static/recipe_app/add_ingredient.js
@@ -1,11 +1,12 @@
 function addIngredient() {
   var newDiv = document.createElement('div');
+  const newFormNumber = document.getElementById('id_ingredient-form-TOTAL_FORMS').value;
 
   newDiv.innerHTML = `
-    <input type="text" name="ingredient-form-${document.getElementById('id_ingredient-form-TOTAL_FORMS').value}-name" placeholder="Ingredient" maxlength="255" id="id_ingredient-form-${document.getElementById('id_ingredient-form-TOTAL_FORMS').value}-name">
+    <input type="text" name="ingredient-form-${newFormNumber}-name" placeholder="Ingredient" maxlength="255" id="id_ingredient-form-${newFormNumber}-name">
     -
-    <input type="text" name="ingredient-form-${document.getElementById('id_ingredient-form-TOTAL_FORMS').value}-measurement" placeholder="Amount" maxlength="255" id="id_ingredient-form-${document.getElementById('id_ingredient-form-TOTAL_FORMS').value}-measurement">
-    <input type="checkbox" name="ingredient-form-${document.getElementById('id_ingredient-form-TOTAL_FORMS').value}-DELETE" onclick="hideParent(this)" id="id_ingredient-form-${document.getElementById('id_ingredient-form-TOTAL_FORMS').value}-DELETE">
+    <input type="text" name="ingredient-form-${newFormNumber}-measurement" placeholder="Amount" maxlength="255" id="id_ingredient-form-${newFormNumber}-measurement">
+    <input type="checkbox" name="ingredient-form-${newFormNumber}-DELETE" onclick="hideParent(this)" id="id_ingredient-form-${newFormNumber}-DELETE">
   `;
 
   document.getElementsByClassName('ingredients-pane')[0].appendChild(newDiv);
