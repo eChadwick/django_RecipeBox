198b07d417201fa5ebdd38d8ca330237478b6e58
eChadwick <en.chadwick@gmail.com>
2024-11-15 10:45:46 -0600

    can add more tag create forms on recipe form

---
+++ RecipeBox/recipe_app/static/recipe_app/add_tag.js
@@ -0,0 +1,12 @@
+function addTag() {
+    var newDiv = document.createElement('p');
+    const newFormNumber = document.getElementById('id_tag-create-form-TOTAL_FORMS').value;
+
+    newDiv.innerHTML = `
+            <label for=id_tag-create-form-${newFormNumber}-tag_name>Tag name:</label>
+            <input type="text" name="tag-create-form-${newFormNumber}-tag_name" maxlength="250" id="id_tag-create-form-${newFormNumber}-tag_name">
+    `
+
+    document.getElementsByClassName('tag-create-pane')[0].appendChild(newDiv);
+    document.getElementById('id_tag-create-form-TOTAL_FORMS').value++;
+}
