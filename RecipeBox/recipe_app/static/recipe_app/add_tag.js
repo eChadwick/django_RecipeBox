07d26a804b9553c314b5154788652eb71706c1c0
eChadwick <en.chadwick@gmail.com>
2024-11-15 13:24:09 -0600

    delete created tags on recipe form

--- RecipeBox/recipe_app/static/recipe_app/add_tag.js
+++ RecipeBox/recipe_app/static/recipe_app/add_tag.js
@@ -5,6 +5,7 @@ function addTag() {
     newDiv.innerHTML = `
             <label for=id_tag-create-form-${newFormNumber}-tag_name>Tag name:</label>
             <input type="text" name="tag-create-form-${newFormNumber}-tag_name" maxlength="250" id="id_tag-create-form-${newFormNumber}-tag_name">
+            <input type="checkbox" onclick="hideParent(this)">
     `
 
     document.getElementsByClassName('tag-create-pane')[0].appendChild(newDiv);
