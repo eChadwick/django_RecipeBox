0cdd1627b0fd2165f3df43ffc3d3529df7f03e86
Eric Chadwick <en.chadwick@gmail.com>
2025-05-05 14:07:14 -0500

    register tags with admin page

--- recipe_app/admin.py
+++ recipe_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from recipe_app.models import Recipe, Ingredient, RecipeIngredient
+from recipe_app.models import Recipe, Ingredient, RecipeIngredient, Tag
 
 # Register your models here.
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
 admin.site.register(RecipeIngredient)
+admin.site.register(Tag)
