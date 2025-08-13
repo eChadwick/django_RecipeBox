3f874cb0dd33cc5c49ecf673fa197a9d4c124827
Eric Chadwick <en.chadwick@gmail.com>
2025-08-13 18:57:25 -0500

    fix ingredient name/amount ordering in js added ingredients

--- recipe_app/static/recipe_app/add_ingredient.js
+++ recipe_app/static/recipe_app/add_ingredient.js
@@ -3,9 +3,9 @@ function addIngredient() {
   const newFormNumber = document.getElementById('id_ingredient-form-TOTAL_FORMS').value;
 
   newDiv.innerHTML = `
-    <input type="text" name="ingredient-form-${newFormNumber}-name" placeholder="Ingredient" maxlength="255" id="id_ingredient-form-${newFormNumber}-name">
-    -
     <input type="text" name="ingredient-form-${newFormNumber}-measurement" placeholder="Amount" maxlength="255" id="id_ingredient-form-${newFormNumber}-measurement">
+    -
+    <input type="text" name="ingredient-form-${newFormNumber}-name" placeholder="Ingredient" maxlength="255" id="id_ingredient-form-${newFormNumber}-name">
     <input type="checkbox" name="ingredient-form-${newFormNumber}-DELETE" onclick="hideParent(this)" id="id_ingredient-form-${newFormNumber}-DELETE" style="display: none">
     <label for="id_ingredient-form-${newFormNumber}-DELETE" class="delete-label">
       <img src="/static/icons/icons8-minus-48.png">
